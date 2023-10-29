d75be8c7238b0d392c656e1da40a05f7101479e6
Jyotirmay Barman <jyotirmay@developerzilla.com>
2023-10-29 10:22:03 +0530

    refactor: move adapters to db dir

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,18 +1,18 @@
-import { DatabaseAdapter } from "../adapters";
+import { DatabaseAdapter } from "../database/adapters";
 import { CreateUserType } from "../types/create-user.type";
 import { InitConfigType } from "../types/init-config.type";
 
-export class Auth{
-    private adaptor;
-    constructor(data: InitConfigType) {
-        this.adaptor = new DatabaseAdapter(data);
-    }
+export class Auth {
+  private adaptor;
+  constructor(data: InitConfigType) {
+    this.adaptor = new DatabaseAdapter(data);
+  }
 
-    async register(data: CreateUserType) {
-        console.log("In register method");
-        const user = await this.adaptor.findUser({
-            first_name: data.first_name
-        })
-        return user;
-    }
+  async register(data: CreateUserType) {
+    console.log("In register method");
+    const user = await this.adaptor.findUser({
+      first_name: data.first_name,
+    });
+    return user;
+  }
 }
